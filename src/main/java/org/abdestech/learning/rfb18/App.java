6a5ae1d2ec054b308509b3fdbc468ae4ad849739
a0z9 <a0z9@rambler.ru>
2018-08-17 10:24:30 +0300

    1st commit

---
+++ src/main/java/org/abdestech/learning/rfb18/App.java
@@ -0,0 +1,31 @@
+package org.abdestech.learning.rfb18;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        /**
+         * 
+         * Домашня работа на выходные дни.
+         * TODO:
+         *
+         * 1. Создать обобщённый класс Summator
+         *    складывающий 2 любых числа.
+         *
+         * 2. Создать классы Book и Library.
+         *    Book должен содержать поля author, title, year, ISBN.
+         *    В классе Book нужно переопределить метод toString.
+         *    В класс Library дабавить произвольное
+         *    количество книг; В качестве реализации хранилища использовать
+         *    коллекцию ArrayList.
+         *
+         *    Проверить работу классов.
+         *
+         *
+          */
+
+
+    }
+}
