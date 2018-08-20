e79f35559dc909302375a294ad343bb239532767
Daniil <38988408+TIE-Bear@users.noreply.github.com>
2018-08-20 09:52:53 +0600

    Update Library.java
    Add ArrayList  and addBook method.

--- src/main/java/org/abdestech/learning/rfb18/Library.java
+++ src/main/java/org/abdestech/learning/rfb18/Library.java
@@ -1,29 +1,63 @@
 package org.abdestech.learning.rfb18;
 
+import java.util.ArrayList;
+
 public class Library  {
 
     private String LName, LAddress;
+    private int volume;
+    private int BkIndex = 0;
+    ArrayList<Book> books;
 
-    Library(String LName, String LAddress) {
+
+    Library(String LName, String LAddress, int volume) {
+
         this.LName = LName;
         this.LAddress = LAddress;
+        this.volume = volume;
+        books = new ArrayList<>(this.volume);
+    }
+
+    Library(String LName, String LAddress) {
+
+        this.LName = LName;
+        this.LAddress = LAddress;
+        volume = 100;
+        books = new ArrayList<>(volume);
+    }
+
+    Library() {
+
+        this.LName = "Ddemin's Library";
+        this.LAddress = "Omsk city, Aviagorodok str, 9";
+        volume = 10;
+        books = new ArrayList<>(volume);
     }
 
     public String getLName() {
+
         return LName;
     }
 
-    public void setLName(String LName) {
+    void setLName(String LName) {
+
         this.LName = LName;
     }
 
     public String getLAddress() {
+
         return LAddress;
     }
 
-    public void setLAddress(String LAddress) {
+    void setLAddress(String LAddress) {
+
         this.LAddress = LAddress;
     }
 
+    public boolean addBook(String BTitle, String BAuthor, int BYear) throws OutFromFutureException {
+        books.add(BkIndex, new Book(BTitle, BAuthor, BYear));
+        return true;
+    }
 
 }
+
