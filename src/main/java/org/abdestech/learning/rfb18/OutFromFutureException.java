513c2faf7ad8d871b5f0d0045a4d238f7915fe62
Daniil <38988408+TIE-Bear@users.noreply.github.com>
2018-08-31 18:30:15 +0600

    Update OutFromFutureException.java
    Added constructor and "extends Exception".

--- src/main/java/org/abdestech/learning/rfb18/OutFromFutureException.java
+++ src/main/java/org/abdestech/learning/rfb18/OutFromFutureException.java
@@ -1,4 +1,8 @@
 package org.abdestech.learning.rfb18;
 
-public class OutFromFutureException {
+public class OutFromFutureException extends Exception{
+    OutFromFutureException(short years) {
+        System.out.println("Указан год издания больше текущего: " + years);
+    }
 }
+
